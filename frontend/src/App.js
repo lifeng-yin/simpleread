ca30b17698dc123741b88b0bc8bcaa4708bf439c
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-07-19 16:05:18 -0400

    index.css, install bootstrap icons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,11 @@ import logolarge from './logos/SIMPLEREAD_LOGO.png'
 const App = () => {
   return (
     <>
-      <img className = 'center' src = {logolarge} style = {{width: '10%', marginTop: '6%'}}></img>
-      <h1>lmao</h1>
+      <img className = 'center' src = {logolarge} style = {{width: '15%', marginTop: '3%'}}></img>
+      <h1 className = 'title' style = {{color: '#506491', fontWeight: '900', marginTop: '-2%'}}>SimpleRead</h1>
+      <h1 style = {{color: '#a48dad'}}>Book reviewing made simple</h1>
+      <button className = 'center' style = {{marginTop: '3%'}}>Review A Book</button>
+      <button className = 'center' style = {{marginTop: '1%'}}>Read Some Reviews</button>
     </>
   )
 }
