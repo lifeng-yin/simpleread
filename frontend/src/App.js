6bc05c7a57db2bba94b31e79f7024b328c3ab682
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-07-19 16:25:02 -0400

    Home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,17 @@ import logolarge from './logos/SIMPLEREAD_LOGO.png'
 const App = () => {
   return (
     <>
-      <img className = 'center' src = {logolarge} style = {{width: '15%', marginTop: '3%'}}></img>
+      <img alt = 'logo' className = 'center' src = {logolarge} style = {{width: '15%', marginTop: '3%'}}></img>
       <h1 className = 'title' style = {{color: '#506491', fontWeight: '900', marginTop: '-2%'}}>SimpleRead</h1>
       <h1 style = {{color: '#a48dad'}}>Book reviewing made simple</h1>
-      <button className = 'center' style = {{marginTop: '3%'}}>Review A Book</button>
-      <button className = 'center' style = {{marginTop: '1%'}}>Read Some Reviews</button>
+      <button className = 'center' style = {{marginTop: '3%'}}>
+        Review A Book
+        <i class="bi bi-book"></i>
+      </button>
+      <button className = 'center' style = {{marginTop: '1%'}}>
+        Read Some Reviews
+        <i class="bi bi-list-stars"></i>
+      </button>
     </>
   )
 }
