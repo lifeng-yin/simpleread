f56b51ef8810332985c78af80adf4f60acc1d4d9
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-10-05 12:45:48 +0000

    fix routing for netlify because li feng didn't commit

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,43 +1,41 @@
-import React from 'react'
-import logolarge from './logos/SIMPLEREAD_LOGO.png'
-import Hamburger from "./components/navbar/Hamburger/Hamburger.js"
-import CustomLink from "./components/navbar/CustomLink/CustomLink.js"
-import AccountButton from './components/navbar/AccountButton/AccountButton'
+import React from "react";
+import logolarge from "./logos/SIMPLEREAD_LOGO.png";
+import Hamburger from "./components/navbar/Hamburger/Hamburger.js";
+import CustomLink from "./components/navbar/CustomLink/CustomLink.js";
+import AccountButton from "./components/navbar/AccountButton/AccountButton";
 
 const Navbar = () => {
-    
   return (
     <nav>
+      <ul id="logo">
+        <CustomLink to="/">
+          <img
+            src={logolarge}
+            alt="logo"
+            style={{ width: "100%", minWidth: "70px", listStyleType: "none" }}
+          ></img>
+        </CustomLink>
+      </ul>
 
-        <ul id = 'logo'>
-          <CustomLink to = '/simpleread/'>
-            <img src = {logolarge} alt = 'logo' style = {{width: '100%', minWidth: '70px', listStyleType: 'none'}}></img>
-          </CustomLink>
-        </ul>
+      <ul className="desktop nav__links">
+        <CustomLink to="/explore">Explore</CustomLink>
 
-        <ul className="desktop nav__links">
-          
-          <CustomLink to = '/simpleread/explore'>Explore</CustomLink>
-          
-          <p className = 'navspacer'>{' | '}</p>
-          
-          <CustomLink to = '/simpleread/add'>Add Review</CustomLink>
-          
-          <p className = 'navspacer'>{' | '}</p>
-          
-          <CustomLink to = '/simpleread/about'>About</CustomLink>
-          
-          <p className = 'navspacer'>{' | '}</p>
-          
-          <AccountButton />
-          
-        </ul>
-        
-        <Hamburger />
+        <p className="navspacer">{" | "}</p>
 
+        <CustomLink to="/add">Add Review</CustomLink>
+
+        <p className="navspacer">{" | "}</p>
+
+        <CustomLink to="/about">About</CustomLink>
+
+        <p className="navspacer">{" | "}</p>
+
+        <AccountButton />
+      </ul>
+
+      <Hamburger />
     </nav>
-  )
-}
+  );
+};
 
-
-export default Navbar
+export default Navbar;
