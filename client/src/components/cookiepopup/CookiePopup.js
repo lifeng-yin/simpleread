ab6c29b394d6c192d874ab1e30c23e0d0cb12903
Jeffrey Zang <zangj4548@wrdsb.ca>
2022-10-13 13:57:34 +0000

    style and add button

--- client/src/components/cookiepopup/CookiePopup.js
+++ client/src/components/cookiepopup/CookiePopup.js
@@ -1,14 +1,18 @@
-import './CookiePopup.scss'
+import "./CookiePopup.scss";
 
 const CookiePopup = () => {
-    return (
-        <div id='popup'>
-            <div>
-                <h2>Cookies</h2>
-                <p>SimpleRead uses cookies to make the most of your experience.</p>
-                            
-            </div>
-        </div>
-    )
-}
-export default CookiePopup
+  return (
+    <div id="popup">
+      <div>
+        <h2>Cookies</h2>
+        <p>
+          SimpleRead uses cookies to keep track of your accounts and signins.{" "}
+        </p>
+        <button onClick = {() => {
+            document.getElementById('popup').style.display = 'none'
+        }}>Ok</button>
+      </div>
+    </div>
+  );
+};
+export default CookiePopup;
